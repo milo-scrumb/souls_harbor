6768321df305da81601ee6e62502c9806d65af34
creator3000 <adrianthesuper200@gmail.com>
2020-05-02 16:02:04 -0700

    add text to homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-    Souls Harbor Website
-  </q-page>
+   <p> Souls Harbor Website </p>
+   <p> Souls Harbor church </p>
+   <p> Your community anchor </p>
+   <p> Soul Harbor is a Home Fellowship Church given to close and personal relationships.Built to encourage, grow together, to fight for one another and to find peace and confidence together in the will of God  </p>
+     </q-page>
 </template>
 
 <script>
